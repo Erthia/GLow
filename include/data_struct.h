12d5c26d94e7102a7a4aec00e8872fafc61ee7b9
Erthia <catemi3@orange.fr>
2018-05-07 12:40:35 +0200

    Data struct

---
+++ include/data_struct.h
@@ -0,0 +1,18 @@
+#ifndef DATA_STRUCT_H /* si la variable n'est pas définie, le code est pris en compte */
+#define DATA_STRUCT_H /* on défini la variable mentionnée ci-dessus */
+
+typedef struct{
+	float x,y;
+}Coord;
+
+typedef struct{
+	Coord min, max;
+	char type; /* 'P':player - 'e':ennemy - 'o':obstacle */
+}Objects;
+
+typedef struct{
+	Coord min, max;
+	char dir; /* 'N':North - 'S':South - 'E':East - 'W':West */
+}Projectile
+
+#endif
